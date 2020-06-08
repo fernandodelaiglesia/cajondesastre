daa2ffbcf9a1e2f60343a4aafb2ae4b7f59a7014
Fernando <fernandodelaiglesia@users.noreply.github.com>
2020-06-08 14:45:31 +0200

    uso local de ACR

---
+++ FederatedKubernetes/NonFederated/createsecret.sh
@@ -0,0 +1,1 @@
+kubectl create secret docker-registry fernandoregistry-local --namespace webtonto-local  --docker-server=https://fernandoregistry.azurecr.io --docker-username=USERNAME --docker-password=PASSWORD
